f71ae394740c62e9989b54d938fb11085ec18380
wongbenj <wongbenj@grinnell.edu>
2017-01-27 03:00:32 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,93 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum array
+  if array.empty?
+    return 0
+  else
+    sum = 0
+    array.next {|x| sum += x}
+    return sum
+  end
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum array
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr.sort! 
+    return arr[arr.length - 1] + arr[arr.length - 2]
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? array, n
+  if array.empty? && n == 0
+    return true
+  else
+    arr.combination(2).any? {|a, b| a + b == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+  	return false
+  elsif ( /[a-z]/ =~ s[0].downcase && /[^aeiou]/ =~ s[0].downcase)
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if /^[01]*(00)$/.match(s) || s == "0"
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+  def initialize isbn price
+   @isbn=isbn  
+   @price=price  
+     if(@isbn===""||@price<=0)
+        raise ArgumentError  
+     end  
+   end  
+  
+  #setter and getter for isbn and price  
+  def set_isbn(isbn)  
+    @isbn=isbn  
+    if(@isbn==="")  
+      raise ArgumentError  
+    end  
+  end  
+  
+  def set_price(price)  
+    @price=price  
+    if(@price<=0)  
+      raise ArgumentError  
+    end  
+  end  
+  
+  def get_isbn  
+    puts @isbn  
+  end  
+  
+  def get_price  
+    puts @price  
+  end  
+  
+  def price_as_string 
+    puts "$" + format("%.2f", @price)  
+  end  
+end  
